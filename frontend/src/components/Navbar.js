f49c1986c86a12a3737d7837dac3cec674fd8095
Rashaad Ratliff-Brown <rashaad.ratliffbrown@duke.edu>
2023-02-06 20:52:42 -0500

    updating nav bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,18 +3,23 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from "../assets/boost.png"
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={logo}></img></Navbar.Brand>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <Navbar.Collapse id="basic-navbar-nav" className="flex-column align-items-end">
+          <Nav>
+            <Nav.Link href="#content">Content</Nav.Link>
+            <Nav.Link href="#scouting">Scouting</Nav.Link>
+            <Nav.Link href="#boostVision">boostVision</Nav.Link>
+            <Nav.Link href="#team">Team</Nav.Link>
+            <Nav.Link href="#sales">SALES</Nav.Link>
+            {/*<NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -24,7 +29,7 @@ function NavBar() {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+  </NavDropdown>*/}
           </Nav>
         </Navbar.Collapse>
       </Container>
