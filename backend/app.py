ca2c573cf9d5ee80eabf02735d08efb15d25f151
Vicki Nomwesigwa <vickinomwesigwa@Vickis-Air.lan>
2023-02-16 20:19:31 -0500

    fetch data and return in list

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,55 @@
-from flask import Flask
+from flask import Flask, jsonify, abort, request
+
+import boto3
+import pandas as pd
+import os
 
 app = Flask(__name__)
+
+
+def get_data():
+    AWS_S3_BUCKET = os.environ.get("AWS_S3_BUCKET")
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    )
+
+    try:
+        response = s3_client.get_object(
+            Bucket=AWS_S3_BUCKET, Key="sample_results.csv"
+        )
+
+        status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+    except Exception as e:
+        print(e, "Ooooppsss something happened")
+
+    if status == 200:
+        print(f"Successful S3 get_object response. Status - {status}")
+        standings = pd.read_csv(response.get("Body"))
+        return standings
+    else:
+        print(
+            f"Unsuccessful S3 get_object response. Status - {status}"
+        )
+        return None
+
 
 @app.route("/")
 def hello_world():
     return "<p>Welcome to boost dashboard</p>"
+
+
+# get products and add products
+@app.route("/api/standings", methods=["GET"])
+def fetch_standings():
+    """returns all standings"""
+    standings_df = get_data().to_dict(orient="records")
+    
+    if standings_df:
+        return jsonify({"standings": standings_df}), 200
+    else:
+        return jsonify({"message": "There are no standings"}), 404
---
+++ frontend/src/api/AxiosApi.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+
+class AxiosAPi{
+    constructor(){
+        this._host = process.env.REACT_APP_API_URL;
+        this._apiVersion = process.env.REACT_APP_MAIN_VERSION || 'api/';
+    }
+
+    createInstance = (baseURL) => {
+        const axiosInstance = axios.create({
+            baseURL,
+            headers: {'Content-Type': 'application/json'},
+          });
+        
+        axiosInstance.interceptors.response.use(
+          (response) => response,
+          (error) => {
+            if(error.response){
+                if(error.response.status === 403) {
+                    window.location.href = `${window.location.origin}/`;
+                }
+                return error.response 
+            } else {
+                return Promise.reject(error)
+            }
+          }
+          );
+          return axiosInstance;
+    }
+
+    get host() {
+        return this._host;
+      }
+    
+    /**
+     * @param {method} method - method (GET, POST, UPDATE, DELETE)
+     * @param {service} service to be accessed in the backend for example user or forms
+     * @param {endpoint} endpoint for example "/login", "/createUser", "deleteForms"
+     * @param {queries} queries for example "?name=James&age=2" or "?number=234"
+     * @param {options} options used by axios request 
+     *      { 
+     *          headers: {
+     *              token: "<authToken>",
+     *              'Content-Type': 'application/json',
+     *              ...other headers,
+     *              }
+     *      }
+     * */ 
+    async apiRequest(method='GET', service='', endpoint, data=null, queries='', options={}){
+        const baseURL = `${this.host}/${this._apiVersion}/`;
+        const axiosInstance = this.createInstance(baseURL);
+        let url = service;
+        url = endpoint ? `${service}/${endpoint}` : url;
+        url = queries ? `${url}/${queries}`: url;
+
+        const configs = {
+            method,
+            url,
+            data,
+            ...options
+        };
+        if(authenticated){
+            const token = localStorage.getItem('token')
+            configs.headers = { ...configs.headers, "Authorization": `Bearer ${token}`}
+        }
+
+        return await axiosInstance(configs);
+    }
+}
+
+const ApiHandler = new AxiosAPi();
+
+export default ApiHandler;
