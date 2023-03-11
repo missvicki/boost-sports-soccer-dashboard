c88ccd8c2c69baf18876edc4b00c161dbe9535e5
Victor Nomwesigwa <nvbethany479@gmail.com>
2023-03-10 20:24:29 -0500

    Ft dropdown filter table (#3)
    * add gender dropdown
    
    * move dropdowns
    
    ---------
    
    Co-authored-by: Vicki Nomwesigwa <vickinomwesigwa@Vickis-MacBook-Air.local>
    Co-authored-by: Vicki Nomwesigwa <vickinomwesigwa@Vickis-Air.lan>

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -2,9 +2,14 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 
 import Table from 'react-bootstrap/Table';
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownDown from './DropdownClass';
 import '../styles/table.css';
 import resultsActions from '../redux/actions/results.actions';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Results() {
     const dispatch = useDispatch();
@@ -21,61 +26,45 @@ function Results() {
 
     return (
         <div>
-            <Table responsive className='results-table' size="sm">
-                <thead>
-                    <tr>
-                        <th>Team Standings</th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th>
-                            <Dropdown>
-                                <Dropdown.Toggle variant="" id="dropdown-basic">
-                                    Season / Year
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item href="#/action-1">2020</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-2">2021</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">2022</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </th>
-                    </tr>
-                    <tr>
-                        <th># &#9653; </th>
-                        <th>TEAM NAME</th>
-                        <th>MP</th>
-                        <th>W</th>
-                        <th>D</th>
-                        <th>L</th>
-                        <th>G</th>
-                        <th>PTS</th>
-                        <th>FORM</th>
-                    </tr>
-                </thead>
+            <Container>
+                <Row>
+                    <Col><h1>Team Standings</h1></Col>
+                </Row>
+                <Row>
+                    <Col><DropdownDown name="Gender" itemList={["Men", "Women"]} /></Col>
+                    <Col><DropdownDown name="Season/Year" itemList={[2020, 2021, 2022]} /></Col>
+                    <Col><DropdownDown name="Week" itemList={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]} /></Col>
+                </Row>
+                <Table responsive className='results-table' size="sm">
+                    <thead>
+                        <tr>
+                            <th># &#9653; </th>
+                            <th>TEAM NAME</th>
+                            <th>MP</th>
+                            <th>W</th>
+                            <th>D</th>
+                            <th>L</th>
+                            <th>G</th>
+                        </tr>
+                    </thead>
 
-                <tbody>
-                    {standings.length > 0 &&
-                        standings.map((row, index) => (
-                            <tr key={index}>
-                                <td>{row.rank}</td>
-                                <td><a href="#">{row.team_name}</a></td>
-                                <td>{row.matches_played}</td>
-                                <td>{row.wins}</td>
-                                <td>{row.draws}</td>
-                                <td>{row.losses}</td>
-                                <td>{row.goals}</td>
-                                <td>{row.points}</td>
-                                <td>{row.form}</td>
-                            </tr>
-                        ))}
-                </tbody>
+                    <tbody>
+                        {standings.length > 0 &&
+                            standings.map((row, index) => (
+                                <tr key={index}>
+                                    <td>{row.rank}</td>
+                                    <td><a href="#">{row.team_name}</a></td>
+                                    <td>{row.matches_played}</td>
+                                    <td>{row.wins}</td>
+                                    <td>{row.draws}</td>
+                                    <td>{row.losses}</td>
+                                    <td>{row.goals}</td>
+                                </tr>
+                            ))}
+                    </tbody>
 
-            </Table>
+                </Table>
+            </Container>
         </div>
     );
 }
